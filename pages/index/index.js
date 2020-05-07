e66113a7fdc7baa0e8dc5e8b408a677ae06a08a9
AHAyayaya <694622038@qq.com>
2020-05-07 16:14:41 +0800

    image组件的使用

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,5 +4,22 @@ const app = getApp()
 
 Page({
   data: {
+  },
+  btnChoose(){
+    wx.chooseImage({
+      complete: (res) => {
+        console.log(res)
+      // 1.取出路径
+      const path = res.tempFilePaths[0]
+      // 2.设置路径
+      this.setData({
+        imagePath: path
+      })
+      },
+      
+    })
+  },
+  imagefinished(){
+    console.log(111111)
   }
 })
