c64ecded31e4c1385476d1476a8f33a77a0652de
AHAyayaya <694622038@qq.com>
2020-05-07 16:33:42 +0800

    input组件的使用

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,5 +4,14 @@ const app = getApp()
 
 Page({
   data: {
+  },
+  input(event){
+    console.log('input输入内容',event)
+  },
+  focus(event){
+    console.log('input获取焦点',event)
+  },
+  blur(event){
+    console.log('input失去焦点',event)
   }
 })
