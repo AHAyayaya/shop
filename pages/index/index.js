58b1e138e724a2cfc03dbaf7aa2451e42fec3e5f
AHAyayaya <694622038@qq.com>
2020-05-07 17:41:20 +0800

    条件判断

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,5 +4,30 @@ const app = getApp()
 
 Page({
   data: {
+    isShow: true,
+    score: 98,
+    nowdate: new Date().toLocaleString()
+  },
+  onLoad(){
+    setInterval(() => {
+      this.setData({
+        nowdate: new Date().toLocaleString()
+      })
+    },1000)
+  },
+  btnshow(){
+    this.setData({
+      isShow: !this.data.isShow
+    })
+  },
+  increament(){
+    this.setData({
+      score: this.data.score +5
+    })
+  },
+  decreament(){
+    this.setData({
+      score: this.data.score -5
+    })
   }
 })
