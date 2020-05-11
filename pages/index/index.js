4ebed5a6ac4c4b04c1c2af0456472375dadf3016
AHAyayaya <694622038@qq.com>
2020-05-11 14:53:24 +0800

    网络请求的基本使用

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,5 +4,39 @@ const app = getApp()
 
 Page({
   data: {
+  },
+  onLoad(){
+    // 1.基本网络请求
+    // wx.request({
+    //   url: 'http://152.136.185.210:8000/api/n3/recommend',
+    //   success(r){
+    //     console.log(r)
+    //   }
+    // })
+
+    // 2.带参数的get请求
+    // wx.request({
+    //   url: 'http://152.136.185.210:8000/api/n3/home/data',
+    //   data:{
+    //     type: "sell",
+    //     page: 1
+    //   },
+    //   success(e){
+    //     console.log(e)
+    //   }
+    // })
+
+    // 3.带参数的post请求
+    wx.request({
+      url: 'http://httpbin.org/post',
+      method: 'post',
+      data:{
+        name: 'Chz',
+        age: 22
+      },
+      success(e){
+        console.log(e)
+      }
+    })
   }
 })
