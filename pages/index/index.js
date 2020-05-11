600de18a02607cf0c1d913cf0076af4473367736
AHAyayaya <694622038@qq.com>
2020-05-11 15:46:23 +0800

    微信小程序分享

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,5 +4,12 @@ const app = getApp()
 
 Page({
   data: {
+  },
+  onShareAppMessage(){
+    return {
+      title: 'Gxx',
+      path: '/pages/index/index',
+      imageUrl: 'http://s3.mogucdn.com/mlcdn/c45406/170915_0a93207ci28kelh617k4hh62l65lb_640x960.jpg'
+    }
   }
 })
